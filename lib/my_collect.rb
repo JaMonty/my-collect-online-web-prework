08cb40feeb4e35c30838a0ae2e149cbe0f9e268c
Jason  Montes <jasone.montes@gmail.com>
2019-04-08 20:25:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0 
   collection = [] 
   while i < collection.length 
-  new << yield(collection[c])
+  collection << yield(collection[c])
   i += 1 
 end
   collection
